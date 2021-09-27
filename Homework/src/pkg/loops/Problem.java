25c6dac7fb4d6b015d0ed9fe2a49dbd8e02d90fe
Boyan Bonev <boyanbonev43@gmail.com>
2021-09-27 17:37:10 +0300

    Added BucketCalculator Solution

---
+++ Homework/src/pkg/loops/Problem.java
@@ -0,0 +1,32 @@
+package pkg.loops;
+
+import java.util.Scanner;
+
+public class Problem {
+    public static void main(String[] args){
+        Scanner scanner =  new Scanner(System.in);
+
+        System.out.println("Length?");
+        double length = scanner.nextDouble();
+        System.out.println("Width?");
+        double width = scanner.nextDouble();
+        System.out.println("Height?");
+        double height = scanner.nextDouble();
+
+        double area = length * width + height * length * 2 + height * width * 2;
+
+        int bigBuckets = (int) area / 150;
+
+        double paintLeft = area % 140;
+
+        int smallBuckets = (paintLeft % 30 == 0 ? (int) paintLeft / 30 : (int)  paintLeft / 30 + 1);
+
+        bigBuckets += (smallBuckets >= 4) ? 1 : 0;
+        smallBuckets = (smallBuckets >= 4) ? 0 : smallBuckets;
+
+        System.out.printf("You need %d big buckets", bigBuckets);
+        if(smallBuckets > 0){
+            System.out.printf(" and %d small buckets", smallBuckets);
+        }
+    }
+}

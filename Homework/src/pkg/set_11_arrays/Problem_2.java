4348098686c64e6da370862a3e523d38ea9fe90f
bonevboyan <boyanbonev43@gmail.com>
2021-11-08 17:18:47 +0200

    Finished Set 11

--- Homework/src/pkg/set_11_arrays/Problem_2.java
+++ Homework/src/pkg/set_11_arrays/Problem_2.java
@@ -1,7 +1,41 @@
 package pkg.set_11_arrays;
 
+import java.util.ArrayList;
 public class Problem_2 {
     public static void main(String[] args) {
+        Deck deck = new Deck();
 
+        for (int i = 0; i < 4; i++) {
+            System.out.println(deck.takeACard().toString());
+        }
+    }
+}
+class Deck{
+    private ArrayList<Card> cards;
+    public Deck(){
+        cards = new ArrayList<Card>(52);
+        for (String suit: new String[]{"Spades", "Hearts", "Diamonds", "Clubs"}) {
+            for (String card: new String[]{"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"}) {
+                cards.add(new Card(suit, card));
+            }
+        }
+    }
+    public Card takeACard(){
+        int randomIndex = (int) Math.floor(Math.random() * cards.size());
+        return cards.remove(randomIndex);
+    }
+}
+class Card{
+    public String suit;
+    public String card;
+
+    public Card(String suit, String card) {
+        this.suit = suit;
+        this.card = card;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s of %s", this.card, this.suit);
     }
 }

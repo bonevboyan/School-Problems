db65213a6dcfd70a021ed6ebbe78693b0949ad74
bonevboyan <boyanbonev43@gmail.com>
2021-12-20 16:56:09 +0200

    Started Set 17

---
+++ Homework/src/pkg/set_17_exercises/GeometricObject.java
@@ -0,0 +1,39 @@
+package pkg.set_17_exercises;
+
+import java.util.Date;
+
+public class GeometricObject {
+    private String color;
+    private boolean filled;
+    private Date dateCreated;
+
+    public GeometricObject() {
+        dateCreated = new Date();
+    }
+
+    public GeometricObject(String color, boolean filled) {
+        this();
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+}
---
+++ Homework/src/pkg/set_17_exercises/Rectangle.java
@@ -0,0 +1,6 @@
+package pkg.set_17_exercises;
+
+public class Rectangle {
+    private double width;
+    private double height;
+}

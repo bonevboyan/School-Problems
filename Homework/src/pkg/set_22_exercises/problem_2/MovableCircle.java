9f80e0cf14315b514ff21eb1606987d89aded241
bonevboyan <boyanbonev43@gmail.com>
2022-01-16 17:24:25 +0200

    Minor fix

--- Homework/src/pkg/set_22_exercises/problem_2/MovableCircle.java
+++ Homework/src/pkg/set_22_exercises/problem_2/MovableCircle.java
@@ -11,22 +11,22 @@ public class MovableCircle implements Movable{
 
     @Override
     public void moveUp() {
-        center.y += center.ySpeed;
+        center.moveUp();
     }
 
     @Override
     public void moveDown() {
-        center.y -= center.ySpeed;
+        center.moveDown();
     }
 
     @Override
     public void moveLeft() {
-        center.x -= center.xSpeed;
+        center.moveLeft();
     }
 
     @Override
     public void moveRight() {
-        center.x += center.xSpeed;
+        center.moveRight();
     }
 
     @Override

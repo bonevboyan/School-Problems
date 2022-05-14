26a9952aad70e8e52df004b23824a3d2c1c72e6c
bonevboyan <boyanbonev43@gmail.com>
2022-05-14 18:23:06 +0300

    Update HeapSort.java

--- Homework/src/term_2/set_16_heap_sort/HeapSort.java
+++ Homework/src/term_2/set_16_heap_sort/HeapSort.java
@@ -1,7 +1,5 @@
 package term_2.set_16_heap_sort;
 
-import java.util.Comparator;
-
 public class HeapSort {
 
     /**
@@ -20,17 +18,17 @@ public class HeapSort {
             list[i] = heap.remove();
     }
 
-    public static <E> void heapSort(E[] list, Comparator<? super E> comparator) {
-        Heap<E> heap = new Heap<>();
-
-        // Add elements to the heap
-        for (int i = 0; i < list.length; i++)
-            heap.add(list[i]);
-
-        // Remove elements from the heap
-        for (int i = list.length - 1; i >= 0; i--)
-            list[i] = heap.remove();
-    }
+//    public static <E> void heapSort(E[] list, Comparator<? super E> comparator) {
+//        Heap<E> heap = new Heap<>();
+//
+//        // Add elements to the heap
+//        for (int i = 0; i < list.length; i++)
+//            heap.add(list[i]);
+//
+//        // Remove elements from the heap
+//        for (int i = list.length - 1; i >= 0; i--)
+//            list[i] = heap.remove();
+//    }
 
     /**
      * A test method

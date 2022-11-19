298f3701d8ae8742c07c4f752be48320728815c6
Boyan Bonev <boyanbonev43@gmail.com>
2022-11-19 17:13:55 +0200

    Add constraints and insert exercises

---
+++ Homework/src/term_3/set_06_exercises/shopSystemInsertion.sql
@@ -0,0 +1,36 @@
+INSERT INTO Cities VALUES 
+(1, 'Sofia', '', ''),
+(2, 'Plovdiv', '', ''),
+(3, 'Varna', '', ''),
+(4, 'Burgas', '', ''),
+(5, 'Ruse', '', ''),
+(6, 'Pleven', '', ''),
+(7, 'Shumen', '', '')
+
+INSERT INTO Clients VALUES
+(1, 'Ivan', 'Petrov', '9786534231', 1, 'ul. Prolet 18', '0888888881'),
+(2, 'Nikola', 'Popov', '8786534244', 3, 'ul. Mir 8', '0888888882'),
+(3, 'Stefka', 'Stoyanova', '7786534334', 4, 'ul. Dunav 21', '0888888883'),
+(4, 'Maria', 'Doneva', '8786535233', 6, 'ul. Evropa 3', '0888888884'),
+(5, 'Georgi', 'Mitev', '9786544234', 1, 'ul. Malina 5', '0888888885'),
+(6, 'Petar', 'Nikolov', '7786634232', 7, 'ul. Ivan Vazov 45', '0888888886'),
+(7, 'Pavel', 'Popov', '9787534234', 1, 'ul. Sinchets 65', '0888888887'),
+(8, 'Stoyan', 'Donev', '9796534237', 5, 'ul. Dunav 58', '0888888888'),
+(9, 'Galya', 'Stoyanova', '9886534234', 1, 'bul. Vitosha 15', '0888888889')
+
+INSERT INTO ProductTypes VALUES
+(1, 'Meat'),
+(2, 'Dairy'),
+(3, 'Fruit'),
+(4, 'Vegetable')
+
+INSERT INTO Products VALUES 
+(1, 'Salami', 4.5, 1),
+(2, 'Tomatoes', 2.2, 4),
+(3, 'Cheese', 6.5, 2),
+(4, 'Apple', 1.25, 3),
+(5, 'Yoghurt', 0.86, 2),
+(6, 'Milk', 1.5, 2),
+(7, 'Pork', 8.2, 1),
+(8, 'Cucumber', 1.5, 4),
+(9, 'Peach', 3.2, 3)
